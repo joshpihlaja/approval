b5151cc5c19f3144694c4fdc55c43fc401cda535
rvikander <rvikander@imagetrend.com>
2018-04-13 14:03:48 -0500

    Update

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -1,5 +1,5 @@
 ﻿import { Component } from '@angular/core';
-import { NavController, LoadingController} from 'ionic-angular';
+import { NavController, LoadingController, AlertController} from 'ionic-angular';
 
 import { FormService } from '../form/FormService';
 
@@ -12,7 +12,8 @@ import { JobForm } from '../../models/JobForm';
 export class FormPage {
     job: JobForm;
     constructor(public navCtrl: NavController,
-        public loadingCtrl: LoadingController,
+		public loadingCtrl: LoadingController,
+		public alertCtrl: AlertController,
         private _FormService: FormService    )
     {
         this.job = new JobForm();
@@ -33,5 +34,27 @@ export class FormPage {
  //       });
  //   }
 
+	public approve(): void {
+		let confirm = this.alertCtrl.create({
+			message: 'Form Approved!',
+			buttons: [
+				{
+					text: 'Ok'
+				}
+			]
+		});
+		confirm.present();
+	}
 
+	public deny(): void {
+		let confirm = this.alertCtrl.create({
+			message: 'Form Declined.',
+			buttons: [
+				{
+					text: 'Ok'
+				}
+			]
+		});
+		confirm.present();
+	}
 }
